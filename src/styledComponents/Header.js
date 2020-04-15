5494f4cf3914e49cebaf55715eed2220d82e9acf
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-14 22:56:19 -0500

    changes

--- src/styledComponents/Header.js
+++ src/styledComponents/Header.js
@@ -1,8 +1,90 @@
 import styled from "styled-components";
 
 export default styled.div`
-  background-color: #f49c3e;
-  color: #000;
+  // background-color: #f49c3e;
+  color: #fff;
   font-weight: bold;
   padding: 13px;
+
+  background: rgba(76, 76, 76, 1);
+  background: -moz-linear-gradient(
+    -45deg,
+    rgba(76, 76, 76, 1) 0%,
+    rgba(19, 19, 19, 1) 1%,
+    rgba(89, 89, 89, 1) 12%,
+    rgba(28, 28, 28, 1) 16%,
+    rgba(102, 102, 102, 1) 25%,
+    rgba(0, 0, 0, 1) 50%,
+    rgba(17, 17, 17, 1) 60%,
+    rgba(58, 58, 58, 1) 75%,
+    rgba(71, 71, 71, 1) 80%,
+    rgba(19, 19, 19, 1) 100%
+  );
+  background: -webkit-gradient(
+    left top,
+    right bottom,
+    color-stop(0%, rgba(76, 76, 76, 1)),
+    color-stop(1%, rgba(19, 19, 19, 1)),
+    color-stop(12%, rgba(89, 89, 89, 1)),
+    color-stop(16%, rgba(28, 28, 28, 1)),
+    color-stop(25%, rgba(102, 102, 102, 1)),
+    color-stop(50%, rgba(0, 0, 0, 1)),
+    color-stop(60%, rgba(17, 17, 17, 1)),
+    color-stop(75%, rgba(58, 58, 58, 1)),
+    color-stop(80%, rgba(71, 71, 71, 1)),
+    color-stop(100%, rgba(19, 19, 19, 1))
+  );
+  background: -webkit-linear-gradient(
+    -45deg,
+    rgba(76, 76, 76, 1) 0%,
+    rgba(19, 19, 19, 1) 1%,
+    rgba(89, 89, 89, 1) 12%,
+    rgba(28, 28, 28, 1) 16%,
+    rgba(102, 102, 102, 1) 25%,
+    rgba(0, 0, 0, 1) 50%,
+    rgba(17, 17, 17, 1) 60%,
+    rgba(58, 58, 58, 1) 75%,
+    rgba(71, 71, 71, 1) 80%,
+    rgba(19, 19, 19, 1) 100%
+  );
+  background: -o-linear-gradient(
+    -45deg,
+    rgba(76, 76, 76, 1) 0%,
+    rgba(19, 19, 19, 1) 1%,
+    rgba(89, 89, 89, 1) 12%,
+    rgba(28, 28, 28, 1) 16%,
+    rgba(102, 102, 102, 1) 25%,
+    rgba(0, 0, 0, 1) 50%,
+    rgba(17, 17, 17, 1) 60%,
+    rgba(58, 58, 58, 1) 75%,
+    rgba(71, 71, 71, 1) 80%,
+    rgba(19, 19, 19, 1) 100%
+  );
+  background: -ms-linear-gradient(
+    -45deg,
+    rgba(76, 76, 76, 1) 0%,
+    rgba(19, 19, 19, 1) 1%,
+    rgba(89, 89, 89, 1) 12%,
+    rgba(28, 28, 28, 1) 16%,
+    rgba(102, 102, 102, 1) 25%,
+    rgba(0, 0, 0, 1) 50%,
+    rgba(17, 17, 17, 1) 60%,
+    rgba(58, 58, 58, 1) 75%,
+    rgba(71, 71, 71, 1) 80%,
+    rgba(19, 19, 19, 1) 100%
+  );
+  background: linear-gradient(
+    135deg,
+    rgba(76, 76, 76, 1) 0%,
+    rgba(19, 19, 19, 1) 1%,
+    rgba(89, 89, 89, 1) 12%,
+    rgba(28, 28, 28, 1) 16%,
+    rgba(102, 102, 102, 1) 25%,
+    rgba(0, 0, 0, 1) 50%,
+    rgba(17, 17, 17, 1) 60%,
+    rgba(58, 58, 58, 1) 75%,
+    rgba(71, 71, 71, 1) 80%,
+    rgba(19, 19, 19, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#4c4c4c', endColorstr='#131313', GradientType=1 );
 `;
